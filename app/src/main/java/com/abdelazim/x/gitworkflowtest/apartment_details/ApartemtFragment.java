09711f307edabf02ff28642f64cc59b914935e06
Abdelazim <imohammedibrahim01@gmail.com>
2019-01-30 21:59:41 +0200

    add packages

---
+++ app/src/main/java/com/abdelazim/x/gitworkflowtest/apartment_details/ApartemtFragment.java
@@ -0,0 +1,31 @@
+package com.abdelazim.x.gitworkflowtest.apartment_details;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abdelazim.x.gitworkflowtest.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ApartemtFragment extends Fragment {
+
+
+    public ApartemtFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_apartemt, container, false);
+    }
+
+}
